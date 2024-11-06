37a33b6e770f8169488293ef7a1c4dbe96282c85
yanlucr <yanlucr@gmail.com>
2024-11-05 22:32:12 -0400

    EditList Composable

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/editList/EditList.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/editList/EditList.kt
@@ -1,23 +1,17 @@
 package com.example.plaintext.ui.screens.editList
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -30,6 +24,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.graphics.toColorInt
 import com.example.plaintext.data.model.PasswordInfo
 import com.example.plaintext.ui.screens.Screen
 import com.example.plaintext.ui.screens.login.TopBarComponent
@@ -51,7 +46,54 @@ fun EditList(
     navigateBack: () -> Unit,
     savePassword: (password: PasswordInfo) -> Unit
 ) {
+    val headerText = if (isPasswordEmpty(args.password)) "Adicionar nova senha" else "Editar senha"
 
+    val state = EditListState(
+        nomeState = rememberSaveable { mutableStateOf<String>(args.password.name) },
+        usuarioState = rememberSaveable { mutableStateOf<String>(args.password.login) },
+        senhaState = rememberSaveable { mutableStateOf<String>(args.password.password) },
+        notasState = rememberSaveable { mutableStateOf<String>(args.password.notes) },
+    )
+
+    Scaffold(
+        topBar = {
+            Column {
+                TopBarComponent()
+                Text(
+                    text = headerText,
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color("#97ba36".toColorInt()))
+                        .padding(horizontal = 30.dp, vertical = 10.dp)
+                )
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(innerPadding)
+        ) {
+            EditInput(textInputLabel = "Nome", textInputState = state.nomeState)
+            EditInput(textInputLabel = "Usuário", textInputState = state.usuarioState)
+            EditInput(textInputLabel = "Senha", textInputState = state.senhaState)
+            EditInput(
+                textInputLabel = "Notas",
+                textInputState = state.notasState,
+                textInputHeight = 150
+            )
+
+            Button(
+                onClick = {},
+                enabled = true
+            ) {
+                Text("Salvar")
+            }
+
+        }
+    }
 }
 
 
