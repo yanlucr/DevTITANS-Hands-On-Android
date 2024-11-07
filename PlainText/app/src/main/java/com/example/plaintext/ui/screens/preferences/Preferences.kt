31fbbbbaf28ae25b84958f461593d6c86cba66ce
yanlucr <yanlucr@gmail.com>
2024-11-07 13:20:29 -0400

    Fixes do último merge

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/preferences/Preferences.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/preferences/Preferences.kt
@@ -1,30 +1,28 @@
 package com.example.plaintext.ui.screens.preferences
 
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import com.example.plaintext.ui.screens.hello.listViewState
 import com.example.plaintext.ui.screens.login.TopBarComponent
 import com.example.plaintext.ui.screens.util.PreferenceInput
 import com.example.plaintext.ui.screens.util.PreferenceItem
-import com.example.plaintext.ui.viewmodel.PreferencesState
 import com.example.plaintext.ui.viewmodel.PreferencesViewModel
-import kotlinx.coroutines.delay
+
 @Composable
 fun SettingsScreen(navController: NavHostController?,
                    viewModel: PreferencesViewModel = hiltViewModel()
